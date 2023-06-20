fe8ecf0d5cefafbf1b0853432374edc3023cdf70
Hikmah97 <olanipekunhikmahkikelomo@gmail.com>
2023-06-20 11:23:18 +0100

    Budget Application done using HTML,CSS and Bootstrap

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var budgetArr = [];
 
 function addItem() {
-    if (budgetItem.value != "") {
-        budgetArr.push(budgetItem.value)
+    if (budgetItem.value != "" && budgetPrice.value != "" ) {
+        var itemObj = {
+            myItem: budgetItem.value,
+            myPrice: budgetPrice.value
+        }
+        budgetArr.push(itemObj)
+        console.log(budgetArr);
         budgetItem.value = ""
+        budgetPrice.value = ""
         displayItem();
-        console.log(budget);
+        console.log(itemObj);
     }
     else {
-        display.innerHTML = `<h2 style= "margin-left: 0px; color: red"> Please Fill in all spaces</h2>`;
+        display.innerHTML = `<h2 style= "margin-left: 50px; color: red"> Please Fill in all spaces</h2>`;
         console.log("Space cannot be Empty");
     }
 }
@@ -28,8 +34,8 @@ function displayItem() {
         itemTable.innerHTML +=  `
         <tr>
             <td>${i+1}</td>
-            <td>${budgetArr[i].budgetItem}</td>
-            <td>${budgetArr[i].budgetPrice}</td>
+            <td>${budgetArr[i].myItem}</td>
+            <td>${budgetArr[i].myPrice}</td>
             <td>
                 <button class= "btn btn-warning" onclick= "editOne()">Edit</button>
                 <button class= "btn btn-danger" onclick= "deleteOne(${i})">Delete</button>
@@ -43,4 +49,25 @@ function deleteOne(index) {
     console.log(`${index}`);
     budgetArr.splice(index,1)
     displayItem()
+}
+
+function editOne() {
+    var newIndex = editedIndex.value;
+    var newItem = editedDetails.value;
+    console.log(newIndex, newItem);
+    if (editedIndex.value != "" && editedDetails.value != "") {
+        if (budgetArr.length > newIndex-1) {
+           budgetArr.splice(newIndex-1,1,newItem);
+           document.getElementById("modalError").style.display = "none" 
+           document.getElementById("modalSuccess").style.display = "block" 
+        }
+        else {
+            document.getElementById("modalError").style.display = "block" 
+
+        }
+    }
+    else {
+        alert("Please fill in the details");
+    }
+    displayItem();
 }
